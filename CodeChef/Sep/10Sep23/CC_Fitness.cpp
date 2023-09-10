76352a0e8cc192bb3a0fd00d624560b0b19c5d73
Md. Firose Mahmud <105736440+firose-munna@users.noreply.github.com>
2023-09-10 19:19:01 +0600

    Add files via upload

---
+++ CodeChef/Sep/10Sep23/CC_Fitness.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int testCase, i;
+    cin>>testCase;
+    for(i=0; i<testCase; i++){
+        int x;
+        cin>>x;
+
+
+        cout<<x*10<<endl;
+    }
+}
